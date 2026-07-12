f7d809f1ef3d8a43185ecd63a013c9713d745d74
GitHub git update Action <action@github.com>
2026-07-12 17:33:38 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_nissan-z31.h
+++ generated/controllers/generated/signature_nissan-z31.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 140291917
-#define TS_SIGNATURE "rusEFI main.2026.07.12.nissan-z31.140291917"
+#define SIGNATURE_HASH 314467357
+#define TS_SIGNATURE "rusEFI main.2026.07.12.nissan-z31.314467357"
