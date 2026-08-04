f4f0088682e9f4d4993e5d26219097ad9f302cec
GitHub git update Action <action@github.com>
2026-08-04 00:26:40 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_nissan-z31.h
+++ generated/controllers/generated/rusefi_generated_nissan-z31.h
@@ -2274,7 +2274,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.08.03.nissan-z31.3560560776"
+#define TS_SIGNATURE "rusEFI main.2026.08.04.nissan-z31.3560560776"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
